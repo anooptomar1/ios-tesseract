c99b6ceaa79710654ca7f21687a40fe1ff7b2cf9
Anoop tomar <anooptomar@Anoops-macbook.local>
2015-02-16 14:21:41 -0800

    added action sheet logic

--- ios_tesseract/ViewController.swift
+++ ios_tesseract/ViewController.swift
@@ -8,8 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var resultText: UITextView!
+    
+    @IBAction func snapAction(sender: AnyObject) {
+        
+        let imagePickerActionSheet = UIAlertController(title: "Snap/Upload an Image", message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet);
+        
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
+            let cameraButton = UIAlertAction(title: "Take Photo", style: UIAlertActionStyle.Default, handler: {(alert)-> Void in
+                let imagePicker = UIImagePickerController();
+                imagePicker.delegate = self;
+                imagePicker.sourceType = .Camera;
+                self.presentViewController(imagePicker, animated: true, completion: nil);
+            });
+            imagePickerActionSheet.addAction(cameraButton)
+        }
+        
+        let libraryButton = UIAlertAction(title: "Choose from photo library", style: UIAlertActionStyle.Default, handler: {(alert) -> Void in
+            let imagePicker = UIImagePickerController();
+            imagePicker.delegate = self;
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        });
+        
+        imagePickerActionSheet.addAction(libraryButton);
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil);
+        imagePickerActionSheet.addAction(cancelButton);
+        
+        presentViewController(imagePickerActionSheet, animated: true, completion: nil);
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
