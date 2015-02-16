0da032d6fcffc1cad5a6ea4acb572e0606916c6b
Anoop tomar <anooptomar@Anoops-macbook.local>
2015-02-16 13:15:29 -0800

    initial commit

---
+++ ios_tesseract/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_tesseract
+//
+//  Created by Anoop tomar on 2/16/15.
+//  Copyright (c) 2015 devtechie.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
